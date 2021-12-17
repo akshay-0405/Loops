de8dc7adcbf77983f64d116aee31b3411c7dbc8c
Akshay Kolhe <95869029+akshay-0405@users.noreply.github.com>
2021-12-17 18:36:42 +0530

    Created 01-Right Triangle
    This file Prints Right triangle made of 01

---
+++ nestforloop.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int x,y;
+ clrscr();
+ for(x=0;x<6;x++)
+ {
+   for(y=0;y<x;y++)
+   {
+     printf("01");
+   }
+   printf("\n");
+ }
+ getch();
+ }
